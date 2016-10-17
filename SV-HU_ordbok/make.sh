ce7fcfcf17c367cfa2f3c8033f49c1e512981ebe
Tom Szilagyi <tamas.szilagyi@klarna.com>
2016-10-17 20:29:13 +0200

    Use the raw TIFF images (results in sharper output and smaller pdf)

--- SV-HU_ordbok/make.sh
+++ SV-HU_ordbok/make.sh
@@ -4,9 +4,9 @@ NPAGES=1048
 PAPER=a5
 OUTFILE="Ordbok.pdf"
 
-echo Downloading PNG images for all book pages...
-mkdir png; cd png
-for i in `seq -w 1 $NPAGES` ; do wget http://runeberg.org/img/svhu1972/$i.3.png ; mv $i.3.png $i.png ; done
+echo Downloading TIFF images for all book pages...
+mkdir tif; cd tif
+for i in `seq -w 1 $NPAGES` ; do wget http://runeberg.org/img/svhu1972/$i.1.tif ; mv $i.1.tif $i.tif ; done
 cd ..
 
 echo Downloading HTML files containing index information...
@@ -14,9 +14,9 @@ mkdir html; cd html
 for i in `seq -w 1 $NPAGES` ; do wget -nd -nH -P. -l 0 http://runeberg.org/svhu1972/$i.html ; done
 cd ..
 
-echo Converting PNG images to PDF pages...
+echo Converting TIFF images to PDF pages...
 mkdir pdf;
-for i in `seq -w 1 $NPAGES` ; do convert png/$i.png pdf/$i.pdf ; done
+for i in `seq -w 1 $NPAGES` ; do convert tif/$i.tif pdf/$i.pdf ; done
 
 echo Creating PDF bookmarks...
 for i in `seq -w 1 $NPAGES` ; do cat html/$i.html | awk -f getidx.awk ; done | awk -f mkpdfmarks.awk > pdfmarks
